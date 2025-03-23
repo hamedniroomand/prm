7f8f9852cabfdc5521d065e42b12a531f6ce5685
Hamed Niroomand <hamed@niroomand.dev>
2025-03-23 07:08:57 +0330

    refactor: remove logs

--- PMS.Application/Services/AuthService.cs
+++ PMS.Application/Services/AuthService.cs
@@ -2,7 +2,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using PMS.Application.Common;
 using PMS.Application.Models;
@@ -10,7 +9,7 @@ using PMS.Contracts.Exceptions;
 
 namespace PMS.Application.Services;
 
-public class AuthService(UserService userService, ApplicationSettings applicationSettings, ILogger<AuthService> logger)
+public class AuthService(UserService userService, ApplicationSettings applicationSettings)
 {
     public async Task<string> Login(string username, string password)
     {
@@ -35,46 +34,28 @@ public class AuthService(UserService userService, ApplicationSettings applicatio
         }
         catch (Exception ex) when (ex is not NotFoundException && ex is not UnauthorizedException)
         {
-            logger.LogError(ex, "Unexpected error during login");
             throw new Exception("An error occurred during authentication", ex);
         }
     }
 
     private string GenerateJwtToken(User user)
     {
-        try
+        var claims = new[]
         {
-            logger.LogInformation("1");
-            var claims = new[]
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.Sid, user.Username),
-                new Claim("Id", user.Id.ToString())
-            };
-            logger.LogInformation("2");
+            new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Sid, user.Username),
+        };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(applicationSettings.JwtString));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(applicationSettings.JwtString));
-            logger.LogInformation("3");
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
-            logger.LogInformation("4");
+        var token = new JwtSecurityToken(
+            claims: claims,
+            notBefore: DateTime.UtcNow,
+            expires: DateTime.UtcNow.AddDays(30),
+            signingCredentials: credentials
+        );
 
-            var token = new JwtSecurityToken(
-                claims: claims,
-                notBefore: DateTime.UtcNow,
-                expires: DateTime.UtcNow.AddDays(30),
-                signingCredentials: credentials
-            );
-            logger.LogInformation("5");
-
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-            logger.LogInformation($"Generated token: {tokenString.Substring(0, 20)}..."); // Log part of the token
-            return tokenString;
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Error generating JWT token");
-            throw;
-        }
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
