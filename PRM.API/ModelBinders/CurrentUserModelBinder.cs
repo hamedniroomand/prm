7f4d06a3f65611a3d373f1390f968794ab1de18a
Hamed Niroomand <hamed@niroomand.dev>
2025-03-25 01:32:18 +0330

    feat: use extracted id from claim

--- PRM.API/ModelBinders/CurrentUserModelBinder.cs
+++ PRM.API/ModelBinders/CurrentUserModelBinder.cs
@@ -9,10 +9,7 @@ public class CurrentUserModelBinder : IModelBinder
 {
     public Task BindModelAsync(ModelBindingContext bindingContext)
     {
-        if (bindingContext == null)
-        {
-            throw new ArgumentNullException(nameof(bindingContext));
-        }
+        ArgumentNullException.ThrowIfNull(bindingContext);
 
         var httpContext = bindingContext.HttpContext;
         var user = httpContext.User;
@@ -24,7 +21,7 @@ public class CurrentUserModelBinder : IModelBinder
             var userId = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
             var isValid = int.TryParse(userId, out var id);
             {
-                var currentUser = userRepository.GetQuery().FirstOrDefault(u => u.Id == 1);
+                var currentUser = userRepository?.GetQuery().FirstOrDefault(u => u.Id == id);
                 bindingContext.Result = ModelBindingResult.Success(currentUser);
             }
             if (!isValid)
